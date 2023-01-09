d6f0f8d933ee18d62ef3228a35c667896c4706cf
TamaraDimov <tamemojsoska@gmail.com>
2023-01-09 14:38:57 +0100

    add html and js files of the project

---
+++ script.js
@@ -0,0 +1,34 @@
+const bookList = document.querySelector("#book-ul");
+const title = document.querySelector("#input-title");
+const author = document.querySelector("#input-author");
+const buttonAdd = document.querySelector("#add-button");
+const form = document.querySelector(".main-form");
+const books = [];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const book = {
+    title: title.value,
+    author: author.value,
+  };
+  books.push(book);
+  title.value = "";
+  author.value = "";
+
+  const bookItem = document.createElement("li");
+  bookItem.classList.add("book-ul");
+  bookItem.innerHTML = `
+        <p class="books">${book.title}</p>
+        <p class="books">${book.author}</p>
+        <button class="remove">Remove</button>
+        <div class="border">
+    `;
+  bookList.appendChild(bookItem);
+});
+
+// Remove books from the List
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove")) {
+    e.target.parentElement.remove();
+  }
+});
