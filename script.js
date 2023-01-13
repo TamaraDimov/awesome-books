790bc59b740afb06bf24a815cee767fffd53a732
TamaraDimov <tamemojsoska@gmail.com>
2023-01-13 11:22:07 +0100

    Add code from the approved project as starting poiint

--- script.js
+++ script.js
@@ -1,36 +1,91 @@
-const bookList = document.querySelector("#book-ul");
-const title = document.querySelector("#input-title");
-const author = document.querySelector("#input-author");
-const buttonAdd = document.querySelector("#add-button");
-const form = document.querySelector(".main-form");
-const books = [];
+const form = document.querySelector("form");
+const bookList = document.querySelector(".book-list");
+const titleInput = document.querySelector("#titleInput");
+const authorInput = document.querySelector("#authorInput");
+const addBtn = document.querySelector(".add-btn");
+
+class BookFun {
+  constructor() {
+    this.collections = JSON.parse(localStorage.getItem("collections")) || [];
+  }
+
+  removeBook(cur) {
+    const curparent = cur.parentElement;
+    const curidx = Array.from(curparent.parentElement.children).indexOf(
+      curparent
+    );
+    curparent.remove();
+    this.collections = this.collections.filter(
+      (collection, index) => index !== curidx
+    );
+
+    localStorage.setItem("collections", JSON.stringify(this.collections));
+  }
+
+  showBook() {
+    bookList.innerHTML = "";
+    this.collections.forEach((collection, index) => {
+      bookList.innerHTML += `<div id="${index}" class='books'>
+              <p class="book-info">
+                  <span class="title">"${collection.title}" </span>
+                  <span>&nbsp;by&nbsp;</span>
+                  <span class="author">${collection.author}</span>
+              </p>
+              <button class="remove-btn">Remove</button>
+      </div>`;
+    });
+  }
+
+  addBook(book) {
+    this.collections.push(book);
+    localStorage.setItem("collections", JSON.stringify(this.collections));
+  }
+}
+const bookFun = new BookFun();
+bookFun.showBook();
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const book = {
-    title: title.value,
-    author: author.value,
-  };
-  books.push(book);
-  title.value = "";
-  author.value = "";
+  const book = { title: titleInput.value, author: authorInput.value };
 
-  const bookItem = document.createElement("li");
-  bookItem.classList.add("book-ul");
-  bookItem.innerHTML = `
-        <p class="books">${book.title}</p>
-        <p class="books">${book.author}</p>
-        <button class="remove">Remove</button>
-        <div class="border">
-    `;
-  bookList.appendChild(bookItem);
-  localStorage.setItem("books", JSON.stringify(books));
-});
-
-// Remove books from the List
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("remove")) {
-    e.target.parentElement.remove();
-    localStorage.removeItem("books", JSON.stringify(books));
+  if (titleInput.value !== "" && authorInput.value !== "") {
+    addBtn.disabled = false;
+    bookFun.addBook(book);
+    titleInput.value = "";
+    authorInput.value = "";
+    bookFun.showBook(book);
+  } else {
+    addBtn.disabled = true;
   }
 });
+
+bookList.addEventListener("click", (e) => {
+  if (e.target.className.includes("remove-btn")) {
+    const targetEl = e.target;
+    bookFun.removeBook(targetEl);
+  }
+});
+
+// Date and time
+const clockElement = document.getElementById("clock");
+
+const clock = () => {
+  clockElement.textContent = new Date().toString();
+};
+
+setInterval(clock, 1000);
+
+const navMenus = document.querySelectorAll("nav a");
+const contentEls = document.querySelectorAll(".contents");
+
+for (let i = 0; i < navMenus.length; i += 1) {
+  navMenus[i].addEventListener("click", () => {
+    contentEls.forEach((content, idx) => {
+      if (idx !== i && content.className.includes("active")) {
+        content.classList.remove("active");
+      }
+    });
+
+    contentEls[i].classList.add("active");
+  });
+}
